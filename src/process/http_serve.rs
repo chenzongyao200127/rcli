73481fece9d4a5cfc024bd04d2b9d412ca18e202
chenzongyao200127 <2319251792@qq.com>
2024-04-24 16:25:05 +0800

    feature: add tower-http support

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -2,6 +2,7 @@ use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::{routing::get, Router};
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
 
 #[derive(Debug)]
@@ -12,11 +13,12 @@ struct HttpServerState {
 pub async fn process_http_serve(path: PathBuf, port: u16) -> anyhow::Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!("Serving {:?} on Port {:?}", path, port);
-    let state = HttpServerState { path };
-
+    let state = HttpServerState { path: path.clone() };
+    let dir_service = ServeDir::new(path);
     // axum router
     let router = Router::new()
         .route("/*path", get(file_handler))
+        .nest_service("/tower", dir_service)
         .with_state(Arc::new(state));
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
@@ -37,6 +39,10 @@ async fn file_handler(
             format!("File {} is Not Found", p.display()),
         )
     } else {
+        // TODO: test p is a dictionary
+        // if it is a dictionary, list all files/sub-dictionaries
+        // as <li><a href="/path/to/file">file name</a></li>
+        // <html><body><ul>...</ul></body></html>
         match tokio::fs::read_to_string(p).await {
             Ok(content) => {
                 info!("Read {} bytes", content.len());
@@ -50,5 +56,22 @@ async fn file_handler(
                 )
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServerState {
+            path: PathBuf::from("."),
+        });
+        let path = Path("Cargo.toml".to_string());
+        let (status, content) = file_handler(State(state), path).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(!content.is_empty());
+        assert!(content.trim().starts_with("[package]"));
     }
 }
